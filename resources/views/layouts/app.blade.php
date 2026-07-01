879f139407660c94f7d9db43d2ed0aa9202ca561
ReyniotaAzriel <reyniotaa@gmail.com>
2026-07-01 18:48:21 +0700

    Penyesuaian tampilan

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,7 +6,7 @@
         <meta name="csrf-token" content="{{ csrf_token() }}">
         <link rel="icon" type="image/png" href="{{ asset('favicon.png') }}">
 
-        <title>{{ 'CBT-JINK' }}</title>
+        <title>{{ 'Otaa-CBT' }}</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
